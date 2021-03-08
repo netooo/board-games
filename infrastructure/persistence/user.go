f2bc3e0eaae6325070e85dd8cfd9f498fb620046
netooo <kazushi.mitsue@gmail.com>
2021-03-08 23:37:58 +0900

    Add: get user in persistence layer

--- infrastructure/persistence/user.go
+++ infrastructure/persistence/user.go
@@ -22,5 +22,19 @@ func (up userPersistence) Insert(DB *sql.DB, userID, name, email, password strin
 }
 
 func (up userPersistence) GetByUserID(DB *sql.DB, userID string) (*model.User, error) {
-	panic("implement me")
+	row := DB.QueryRow("SELECT * FROM user WHERE user_id = ?", userID)
+	return convertToUser(row)
+}
+
+// row型をuser型に紐づける
+func convertToUser(row *sql.Row) (*model.User, error) {
+	user := model.User{}
+	err := row.Scan(&user.UserID, &user.Name, &user.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
