02668a4bc169d0cc2c8b93da5ce9d80ef710e9d7
netooo <kazushi.mitsue@gmail.com>
2021-03-09 23:34:16 +0900

    Add: get, insert user in usecase layer

--- usecase/user.go
+++ usecase/user.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"database/sql"
+	"github.com/google/uuid"
 	"github.com/netooo/board-games/domain/model"
 	"github.com/netooo/board-games/domain/repository"
 )
@@ -19,4 +20,29 @@ func NewUserUseCase(ur repository.UserRepository) *userUseCase {
 	return &userUseCase{
 		userRepository: ur,
 	}
+}
+
+func (uu userUseCase) GetByUserID(DB *sql.DB, userID string) (*model.User, error) {
+	user, err := uu.userRepository.GetByUserID(DB, userID)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (uu userUseCase) Insert(DB *sql.DB, name, email, password string) error {
+	// 各種パラメータのバリデーションを行う
+
+	userID, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+
+	// domainを介してinfrastructureで実装した関数を呼び出す。
+	// Persistence（Repository）を呼出
+	err = uu.userRepository.Insert(DB, userID.String(), name, email, password)
+	if err != nil {
+		return err
+	}
+	return nil
 }
