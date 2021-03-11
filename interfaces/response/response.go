d5f8f8d0c1f0d971fadc35e67f37952a95764b6e
netooo <kazushi.mitsue@gmail.com>
2021-03-11 23:26:21 +0900

    Modify: response

---
+++ interfaces/response/response.go
@@ -0,0 +1,45 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Success Status Code:200
+func Success(writer http.ResponseWriter, response interface{}) {
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Println(err)
+		InternalServerError(writer, "marshal error")
+		return
+	}
+	_, _ = writer.Write(data)
+}
+
+// BadRequest Status Code:400
+func BadRequest(writer http.ResponseWriter, message string) {
+	httpError(writer, http.StatusBadRequest, message)
+}
+
+// InternalServerError Status Code:500
+func InternalServerError(writer http.ResponseWriter, message string) {
+	httpError(writer, http.StatusInternalServerError, message)
+}
+
+// httpError Other Error Response
+func httpError(writer http.ResponseWriter, code int, message string) {
+	data, _ := json.Marshal(errorResponse{
+		Code:    code,
+		Message: message,
+	})
+	writer.WriteHeader(code)
+	if data != nil {
+		_, _ = writer.Write(data)
+	}
+}
+
+type errorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
