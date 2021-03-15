33e72160c3c6c67c3a87228b6502dd9adeda315b
netooo <kazushi.mitsue@gmail.com>
2021-03-15 23:22:02 +0900

    Add: handler in interface layer

--- interfaces/handler/user.go
+++ interfaces/handler/user.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"github.com/netooo/board-games/interfaces/response"
 	"github.com/netooo/board-games/usecase"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -13,4 +16,55 @@ type UserHandler interface {
 
 type userHandler struct {
 	userUseCase usecase.UserUseCase
+}
+
+type userSignupRequest struct {
+	UserId   string
+	Name     string
+	Email    string
+	Password string
+}
+
+func NewUserHandler(uu usecase.UserUseCase) UserHandler {
+	return &userHandler{
+		userUseCase: uu,
+	}
+}
+
+func (uh userHandler) HandleUserGet(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	// Contextから認証済みのユーザIdを取得
+	userId := request.FormValue("userId")
+
+	// UseCaseレイヤを操作して、ユーザデータ取得
+	user, err := uh.userUseCase.GetByUserId(userId)
+	if err != nil {
+		response.InternalServerError(writer, "Internal Server Error")
+		return
+	}
+
+	// レスポンスに必要な情報を詰めて返却
+	response.Success(writer, user)
+}
+
+func (uh userHandler) HandleUserSignup(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	// リクエストボディを取得
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		response.BadRequest(writer, "Invalid Request Body")
+		return
+	}
+
+	// リクエストボディのパース
+	var requestBody userSignupRequest
+	_ = json.Unmarshal(body, &requestBody)
+
+	// UseCaseの呼び出し
+	err = uh.userUseCase.Insert(requestBody.Name, requestBody.Email, requestBody.Password)
+	if err != nil {
+		response.InternalServerError(writer, "Internal Server Error")
+		return
+	}
+
+	// レスポンスに必要な情報を詰めて返却
+	response.Success(writer, "")
 }
