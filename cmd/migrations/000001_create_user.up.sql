040c689ff7fa76516dfc1232c148773ed986b3a4
netooo <kazushi.mitsue@gmail.com>
2021-03-23 23:48:53 +0900

    Modify: sql file into sql

--- cmd/migrations/000001_create_user.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID'
-    `user_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'ユーザID'
-    `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '氏名'
-    `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'メールアドレス'
-    `password` varchar(255) COLLATE utf8_inicode_ci NOT NULL COMMENT 'パスワード'
-)
