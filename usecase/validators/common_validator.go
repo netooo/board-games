796ffda2132bfdb9d1531f5f79461a4afb7d4857
netooo <kazushi.mitsue@gmail.com>
2021-03-28 23:26:45 +0900

    Add: common validator file

---
+++ usecase/validators/common_validator.go
@@ -0,0 +1,24 @@
+package validators
+
+import "github.com/go-playground/validator"
+
+// CustomValidator
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func MyValidator() Validator {
+	return &CustomValidator{validator: validator.New()}
+}
+
+// Validate validate
+func (cv *CustomValidator) Validate(i interface{}) error {
+	_ = cv.validator.RegisterValidation("password_validation", PasswordValidation)
+	return cv.validator.Struct(i)
+}
+
+func PasswordValidation(fl validator.FieldLevel) bool {
+	// TODO: パスワードバリデーター作成
+	// 		 小文字1文字以上/大文字1文字以上/数字1文字以上/8文字以上
+	return true
+}
