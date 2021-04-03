d127b755202d0ee90d33daa6ef397cccc7840247
netooo <kazushi.mitsue@gmail.com>
2021-04-03 23:11:17 +0900

    Fix: routing settings

--- routing/router.go
+++ routing/router.go
@@ -7,12 +7,13 @@ import (
 func Init() *mux.Router {
 	// Set Routing
 	r := mux.NewRouter()
-	r.Host("localhost") // TODO: 環境によって変えるようにする
-	r.PathPrefix("/api")
-	r.Schemes("http") // TODO: localではhttp, test/productionではhttpsを使う
+	s := r.PathPrefix("/api").
+		Host("localhost"). // TODO: 環境によって変えるようにする
+		Schemes("http").   // TODO: localではhttp, test/productionではhttpsを使う
+		Subrouter()
 
 	// Read Various APIs
-	UserInit(r)
+	UserInit(s)
 
-	return r
+	return s
 }
