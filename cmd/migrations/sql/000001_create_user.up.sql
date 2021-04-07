57254db27a32d787e3e226eed6f3b3c3041bdbf9
netooo <kazushi.mitsue@gmail.com>
2021-04-08 00:01:54 +0900

    Fix: gorm config and migration file

--- cmd/migrations/sql/000001_create_user.up.sql
+++ cmd/migrations/sql/000001_create_user.up.sql
@@ -4,6 +4,9 @@ CREATE TABLE users (
     `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '氏名',
     `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'メールアドレス',
     `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'パスワード',
+    `created_at` TIMESTAMP COMMENT '作成日時',
+    `updated_at` TIMESTAMP COMMENT '更新日時',
+    `deleted_at` TIMESTAMP COMMENT '更新日時',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uniq_users_on_user_id` (`user_id`)
 )
