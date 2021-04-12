afe4e543015f1249bbd56fe38ef59e62a9cf982b
netooo <kazushi.mitsue@gmail.com>
2021-04-12 23:53:25 +0900

    Add: session authentication for handler

--- routing/router.go
+++ routing/router.go
@@ -4,6 +4,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+//
+//func handleFunc(r *mux.Router, path string, fn http.HandlerFunc) *mux.Route {
+//	return r.HandleFunc(path, applicationHandler(fn))
+//}
+//
+//func applicationHandler(fn http.HandlerFunc) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		// セッションの取得
+//		session, err := store.Get(r, SessionName)
+//		if err != nil {
+//			// 不正なセッションだった場合は作り直す
+//			session, err = store.New(r, SessionName)
+//			checkError(err)
+//		}
+//		context.Set(r, ContextSessionKey, session)
+//		// 個別のハンドラー呼び出し
+//		fn(w, r)
+//	}
+//}
+
 func Init() *mux.Router {
 	// Set Routing
 	r := mux.NewRouter()
