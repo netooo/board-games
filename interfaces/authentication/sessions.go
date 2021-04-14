5f0f3c8fbd51ec38957b32a3a783c00448eca949
netooo <kazushi.mitsue@gmail.com>
2021-04-14 23:55:26 +0900

    Add: example code for session init

--- interfaces/authentication/sessions.go
+++ interfaces/authentication/sessions.go
@@ -2,7 +2,26 @@ package authentication
 
 import (
 	"github.com/gorilla/sessions"
+	"net/http"
 	"os"
 )
 
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+const (
+	SessionName       = "session-name"
+	ContextSessionKey = "session"
+)
+
+func sessionInit(writer http.ResponseWriter, request *http.Request) {
+	// Create New Session
+	session := sessions.NewSession(store, SessionName)
+
+	// Session Config
+	store.Options = &sessions.Options{
+		Domain:   "localhost", // とりあえず開発用に
+		Secure:   false,       // とりあえず開発用に
+		HttpOnly: true,
+	}
+
+}
