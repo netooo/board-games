727d1d3f814921706df3c180e8e8dd8bb27ae077
netooo <kazushi.mitsue@gmail.com>
2021-04-26 00:22:11 +0900

    Add: migration file of session table

---
+++ cmd/migrations/sql/000002_create_session.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sessions (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `session_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'セッションID',
+    `data` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'データ',
+    `user_id` bigint(20) unsigned NOT NULL COMMENT 'ユーザID',
+    `created_at` TIMESTAMP COMMENT '作成日時',
+    `updated_at` TIMESTAMP COMMENT '更新日時',
+    `deleted_at` TIMESTAMP COMMENT '更新日時',
+    PRIMARY KEY (`id`),
+    KEY `foreign_sessions_on_user_id` (`user_id`), /* アホなので外部キー張れなかった。誰かお願い。 */
+    UNIQUE KEY `uniq_sessions_on_session_id` (`session_id`),
+    UNIQUE KEY `uniq_sessions_on_user_id` (`user_id`)
+)
