4557d3619402761d732235950c449c3bcd203df3
netooo <kazushi.mitsue@gmail.com>
2021-05-03 23:53:20 +0900

    Add: enum for status in numeron

--- domain/model/numeron.go
+++ domain/model/numeron.go
@@ -6,3 +6,26 @@ type Numeron struct {
 	gorm.Model
 	Status int `json:status`
 }
+
+type Status int
+
+const (
+	Unknown Status = iota // Unknown  == 0
+	Ready                 // Ready    == 1
+	Play                  // Play     == 2
+	Finish                // Finish   == 3
+)
+
+func (s Status) String() string {
+	switch s {
+	case Unknown:
+		return "不明"
+	case Ready:
+		return "準備中"
+	case Play:
+		return "実行中"
+	case Finish:
+		return "終了"
+	}
+	return "未定義"
+}
