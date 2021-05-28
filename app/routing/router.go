ba117257df4559695798bebc4df883c6f5393598
netooo <kazushi.mitsue@gmail.com>
2021-05-28 23:32:58 +0900

    Remove: Host setting

--- app/routing/router.go
+++ app/routing/router.go
@@ -8,8 +8,7 @@ func Init() *mux.Router {
 	// Set Routing
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api").
-		Host("localhost"). // TODO: 環境によって変えるようにする
-		Schemes("http").   // TODO: localではhttp, test/productionではhttpsを使う
+		Schemes("http"). // TODO: localではhttp, test/productionではhttpsを使う
 		Subrouter()
 
 	// Read Various APIs
