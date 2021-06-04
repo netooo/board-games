8889f1a0209d3496dffc142cbb65175d8288688f
netooo <kazushi.mitsue@gmail.com>
2021-06-04 23:28:46 +0900

    Add: create room interface

--- app/interfaces/handler/common.go
+++ app/interfaces/handler/common.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
+	"github.com/netooo/board-games/app/interfaces/response"
 	"github.com/netooo/board-games/app/usecase"
+	"io/ioutil"
 	"net/http"
 )
 
 type CommonHandler interface {
+	HandleRoomCreate(http.ResponseWriter, *http.Request)
 }
 
 type commonHandler struct {
@@ -19,5 +23,15 @@ func NewCommonHandler(cu usecase.CommonUseCase) CommonHandler {
 }
 
 func (ch commonHandler) HandleRoomCreate(writer http.ResponseWriter, request *http.Request) {
+	//TODO: session check
+	//TODO: request_user already join room check
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		response.BadRequest(writer, "Invalid Request Body")
+		return
+	}
+
+	var requestBody userSignupRequest
+	_ = json.Unmarshal(body, &requestBody)
 
 }
