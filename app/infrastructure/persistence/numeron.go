029fafa19ee27b1a5b35591df8bc2a2c2509d31c
netooo <kazushi.mitsue@gmail.com>
2021-06-16 23:41:53 +0900

    Add: numeron persistence

---
+++ app/infrastructure/persistence/numeron.go
@@ -0,0 +1,30 @@
+package persistence
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/netooo/board-games/app/config"
+	"github.com/netooo/board-games/app/domain/model"
+	"github.com/netooo/board-games/app/domain/repository"
+)
+
+type numeronPersistence struct {
+	Conn *gorm.DB
+}
+
+func NewNumeronPersistence(conn *gorm.DB) repository.NumeronRepository {
+	return &numeronPersistence{Conn: conn}
+}
+
+func (np numeronPersistence) CreateRoom() (*model.Numeron, error) {
+	db := config.Connect()
+	defer config.Close()
+
+	numeron := model.Numeron{
+		Status: 0,
+	}
+
+	db.Create(&numeron)
+
+	return &numeron, nil
+}
