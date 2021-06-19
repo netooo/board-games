41b9797df00b84b0b4e1540ef8543501b5fa4bbf
netooo <kazushi.mitsue@gmail.com>
2021-06-19 20:00:37 +0900

    Add: user model in set code api

--- app/infrastructure/persistence/numeron_player.go
+++ app/infrastructure/persistence/numeron_player.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/netooo/board-games/app/config"
+	"github.com/netooo/board-games/app/domain/model"
 	"github.com/netooo/board-games/app/domain/repository"
 )
 
@@ -15,7 +16,7 @@ func NewNumeronPlayerPersistence(conn *gorm.DB) repository.NumeronPlayerReposito
 	return &numeronPlayerPersistence{Conn: conn}
 }
 
-func (npp numeronPlayerPersistence) SetCode(code string) error {
+func (npp numeronPlayerPersistence) SetCode(user *model.User, code string) error {
 	db := config.Connect()
 	defer config.Close()
 
