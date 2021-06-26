37641f2e88b1daa60d1a3d967298ecaa0ea88804
netooo <kazushi.mitsue@gmail.com>
2021-06-26 22:48:44 +0900

    Fix: gorm pointer

--- app/domain/model/numeron_history.go
+++ app/domain/model/numeron_history.go
@@ -5,9 +5,9 @@ import "github.com/jinzhu/gorm"
 type NumeronHistory struct {
 	gorm.Model
 	NumeronId     int `json:numeron_id`
-	Numeron       Numeron
+	Numeron       *Numeron
 	PlayerId      int `json:player_id`
-	NumeronPlayer NumeronPlayer
+	NumeronPlayer *NumeronPlayer
 	Action        string `json:action`
 	Result        string `json:result`
 	Turn          int    `json:turn`
