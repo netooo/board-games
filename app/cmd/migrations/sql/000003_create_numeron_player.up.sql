9d7e36c9a6ed8cf5c005cd5265ebad523e97ab1a
netooo <kazushi.mitsue@gmail.com>
2021-07-26 23:32:01 +0900

    Modify: comment

--- app/cmd/migrations/sql/000003_create_numeron_player.up.sql
+++ app/cmd/migrations/sql/000003_create_numeron_player.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE numeron_players (
     `numeron_id` integer unsigned NOT NULL COMMENT 'ヌメロンルームID',
     `user_id` bigint(20) unsigned NOT NULL COMMENT 'ユーザID',
     `order` tinyint(1) unsigned NOT NULL COMMENT '順番',
-    `code` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '指定番号',
+    `code` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '設定番号',
     `rank` tinyint(1) unsigned NOT NULL COMMENT '順位',
     `created_at` TIMESTAMP COMMENT '作成日時',
     `updated_at` TIMESTAMP COMMENT '更新日時',
