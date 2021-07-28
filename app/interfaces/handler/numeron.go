c0961eef9cb0a5d9e5a5c53cec7b83169db2bef9
netooo <kazushi.mitsue@gmail.com>
2021-07-28 23:48:32 +0900

    Add: hundler in game start

--- app/interfaces/handler/numeron.go
+++ app/interfaces/handler/numeron.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"github.com/netooo/board-games/app/interfaces/authentication"
 	"github.com/netooo/board-games/app/interfaces/response"
@@ -11,6 +12,7 @@ import (
 
 type NumeronHandler interface {
 	HandleRoomCreate(http.ResponseWriter, *http.Request)
+	HandleGameStart(http.ResponseWriter, *http.Request)
 }
 
 type numeronHandler struct {
@@ -58,4 +60,39 @@ func (nh numeronHandler) HandleRoomCreate(writer http.ResponseWriter, request *h
 	}
 
 	response.Success(writer, room)
+}
+
+func (nh numeronHandler) HandleGameStart(writer http.ResponseWriter, request *http.Request) {
+	/* websocketの開設 */
+	socket, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		log.Fatalln("websocketの開設に失敗しました。:", err)
+	}
+
+	user, err := authentication.SessionUser(request)
+
+	if err != nil {
+		// TODO: redirect login form
+		response.Unauthorized(writer, "Invalid Session")
+		return
+	}
+
+	vars := mux.Vars(request)
+	numeronId := vars["id"]
+
+	if numeronId == "" {
+		response.BadRequest(writer, "Invalid Request Body")
+		return
+	}
+
+	//TODO: Check request_user already join other room?
+	// もしやるんだったら Userテーブルに Statusカラムを追加しないといけなさそう
+
+	order, err := nh.numeronUseCase.GameStart(user, socket, numeronId)
+	if err != nil {
+		response.InternalServerError(writer, "Internal Server Error")
+		return
+	}
+
+	response.Success(writer, order)
 }
