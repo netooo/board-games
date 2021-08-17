3a6bfaeeb5fe3d51ea4efaca6c68463310ca9df7
netooo <kazushi.mitsue@gmail.com>
2021-08-17 23:36:18 +0900

    Modify: json in user model

--- app/domain/model/user.go
+++ app/domain/model/user.go
@@ -7,9 +7,9 @@ import (
 
 type User struct {
 	gorm.Model
-	UserId   string `json:user_id`
-	Name     string `json:name`
-	Email    string `json:email`
-	Password string `json:password`
-	Socket   *websocket.Conn
+	UserId   string          `json:"user_id"`
+	Name     string          `json:"name"`
+	Email    string          `json:"email"`
+	Password string          `json:"password"`
+	Socket   *websocket.Conn `json:"-"`
 }
