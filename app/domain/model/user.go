a6a119c974e1b07903bc3ce967c12961569884a6
netooo <kazushi.mitsue@gmail.com>
2021-12-02 09:53:01 +0900

    Fix: extra code

--- app/domain/model/user.go
+++ app/domain/model/user.go
@@ -3,7 +3,6 @@ package model
 import (
 	"github.com/gorilla/websocket"
 	"github.com/jinzhu/gorm"
-	"github.com/netooo/board-games/app/config"
 )
 
 type User struct {
@@ -28,8 +27,8 @@ type Message struct {
 対象ユーザから送られてきたsocketメッセージを受け取る
 */
 func (u *User) Read() {
-	db := config.Connect()
-	defer config.Close()
+	//db := config.Connect()
+	//defer config.Close()
 
 	// websocketからjson形式でメッセージを読み出す。
 	// 読み込みは無限ループで実行される。
@@ -38,14 +37,7 @@ func (u *User) Read() {
 		if err := u.Socket.ReadJSON(&msg); err != nil {
 			break
 		} else {
-			switch msg.Game {
-			case "numeron":
-				var numeron Numeron
-				if err := db.Omit("Join", "Leave", "Players").First(&numeron, msg.Id).Error; err != nil {
-					break
-				}
-				numeron.Read(u, msg.Action, msg.Value)
-			}
+
 		}
 	}
 	_ = u.Socket.Close()
