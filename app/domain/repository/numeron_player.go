0737a9d7902a77df3204a18e69a6f9ccaa181d03
netooo <kazushi.mitsue@gmail.com>
2021-12-03 23:39:43 +0900

    Fix: numeron_players

--- app/domain/repository/numeron_player.go
+++ app/domain/repository/numeron_player.go
@@ -4,5 +4,4 @@ import "github.com/netooo/board-games/app/domain/model"
 
 type NumeronPlayerRepository interface {
 	SetCode(user *model.User, id string, code string) error
-	JoinRoom(user *model.User, id string) error
 }
--- app/routing/numeron_player.go
+++ app/routing/numeron_player.go
@@ -15,5 +15,4 @@ func NumeronPlayerInit(r *mux.Router) {
 	numeronPlayerHandler := handler.NewNumeronPlayerHandler(numeronPlayerUseCase)
 
 	r.HandleFunc("/numerons/{id}/code", numeronPlayerHandler.HandleNumeronSetCode).Methods("POST")
-	r.HandleFunc("/numerons/{id}/join", numeronPlayerHandler.HandleNumeronJoinRoom).Methods("POST")
 }
