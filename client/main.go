8e6db575122b5d419504f50e80f3b3fde7fc3ba8
netooo <kazushi.mitsue@gmail.com>
2021-12-19 23:52:42 +0900

    Add: main.go of client side

--- client/main.go
+++ client/main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	port := "3000"
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
-	log.Printf("Server listening on http://localhost:%s/", port)
-	log.Print(http.ListenAndServe(":"+port, nil))
+	http.Handle("/", http.FileServer(http.Dir("public/")))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
