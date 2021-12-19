ed5e545de8704ddb6f87bdb24bbb1736c1f4d093
netooo <kazushi.mitsue@gmail.com>
2021-12-19 23:51:51 +0900

    Add: apply cors

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/netooo/board-games/app/routing"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
@@ -9,7 +10,8 @@ import (
 func main() {
 	// ルーティングの設定
 	r := routing.Init()
+	c := cors.Default().Handler(r)
 
 	// サーバ起動
-	log.Fatal(http.ListenAndServe(":9000", r))
+	log.Fatal(http.ListenAndServe(":9000", c))
 }
