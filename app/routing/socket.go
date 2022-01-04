cd76b0b2b1122886fef2517234b48f854b5029ad
netooo <kazushi.mitsue@gmail.com>
2022-01-04 18:32:35 +0900

    Add: routing socket

--- app/routing/socket.go
+++ app/routing/socket.go
@@ -2,11 +2,16 @@ package routing
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/netooo/board-games/app/config"
+	"github.com/netooo/board-games/app/infrastructure/persistence"
 	"github.com/netooo/board-games/app/interfaces/handler"
+	"github.com/netooo/board-games/app/usecase"
 )
 
 func SocketInit(r *mux.Router) {
-	socketHandler := handler.NewSocketHandler()
+	socketPersistence := persistence.NewSocketPersistence(config.Connect())
+	socketUseCase := usecase.NewSocketUseCase(socketPersistence)
+	socketHandler := handler.NewSocketHandler(socketUseCase)
 
 	r.HandleFunc("/ws/connect", socketHandler.HandleSocketConnect).Methods("GET")
 	r.HandleFunc("/ws/disconnect", socketHandler.HandleSocketDisconnect).Methods("GET")
