8d7b96f8c49f80c54cdfed478c9c96219dc8d87c
netooo <kazushi.mitsue@gmail.com>
2022-01-13 23:53:34 +0900

    Add: usecase and repository

--- app/domain/repository/room.go
+++ app/domain/repository/room.go
@@ -7,5 +7,6 @@ import (
 type RoomRepository interface {
 	GetRooms() ([]*model.Room, error)
 	CreateRoom(name string, user *model.User) (uint, error)
+	ShowRoom(roomId uint) (*model.Room, error)
 	JoinRoom(roomId uint, user *model.User) error
 }
