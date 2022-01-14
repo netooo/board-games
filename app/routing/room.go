1f30ee5700bee0c49af5f89fd46f78e5c097b5ea
netooo <kazushi.mitsue@gmail.com>
2022-01-14 18:55:37 +0900

    Fix: endpoint of join room API

--- app/routing/room.go
+++ app/routing/room.go
@@ -16,5 +16,5 @@ func RoomInit(r *mux.Router) {
 	r.HandleFunc("/rooms", roomHandler.HandleRoomGet).Methods("GET")
 	r.HandleFunc("/rooms", roomHandler.HandleRoomCreate).Methods("POST")
 	r.HandleFunc("/rooms/{id}", roomHandler.HandleRoomShow).Methods("GET")
-	r.HandleFunc("/rooms/{id}", roomHandler.HandleRoomJoin).Methods("POST")
+	r.HandleFunc("/rooms/{id}/entry", roomHandler.HandleRoomJoin).Methods("POST")
 }
