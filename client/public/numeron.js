f029a4a4b9ed557d870aa1fec2fe2b7f0d3c18b5
netooo <kazushi.mitsue@gmail.com>
2022-01-25 21:59:23 +0900

    Fix: Synchronous processing in show numeron

--- client/public/numeron.js
+++ client/public/numeron.js
@@ -46,6 +46,30 @@ function getNumeron(display_id) {
     });
 }
 
+function showNumeron(display_id) {
+    return new Promise((resolve, reject) => {
+        if (display_id === "") {
+            return
+        }
+        const url = 'http://localhost:8082/api/numerons/' + display_id;
+        const xhr = new XMLHttpRequest()
+        xhr.withCredentials = true
+        xhr.open('GET', url);
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.send();
+
+        xhr.addEventListener('readystatechange', function () {
+            if (this.readyState === this.DONE) {
+                if (this.status === 200) {
+                    return resolve(JSON.parse(this.responseText));
+                } else {
+                    return resolve(null);
+                }
+            }
+        });
+    })
+}
+
 function leaveNumeron(display_id) {
     if (display_id === "") {
         return
@@ -67,17 +91,29 @@ function leaveNumeron(display_id) {
     });
 }
 
-function startGame() {
+function startNumeron(display_id) {
     if (display_id === "") {
         return
     }
 
+    let numeron = showNumeron(display_id)
+    if (numeron === null) {
+        console.log("Error: showNumeron")
+        return null
+    }
+
+    let first_id = numeron['users'][0]['user_id'];
+    let second_id = numeron['users'][1]['user_id'];
     const url = 'http://localhost:8082/api/numerons/'+display_id+'/start';
+    const data = JSON.stringify({
+        first: first_id,
+        second: second_id,
+    });
     const xhr = new XMLHttpRequest()
     xhr.withCredentials = true
     xhr.open('POST', url);
     xhr.setRequestHeader('content-type', 'application/json');
-    xhr.send();
+    xhr.send(data);
 
     xhr.addEventListener('readystatechange', function() {
         if (this.readyState === this.DONE) {
