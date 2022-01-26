ad049238096938367862528fb24cd0b2064bf8ee
netooo <kazushi.mitsue@gmail.com>
2022-01-26 17:21:02 +0900

    Remove: numeron_player layers

--- app/domain/repository/numeron_player.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/netooo/board-games/app/domain/model"
-
-type NumeronPlayerRepository interface {
-	SetCode(user *model.User, id string, code string) error
-}
