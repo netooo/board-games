1204b385b3420619d5879dc7b14513c54167f4f7
netooo <kazushi.mitsue@gmail.com>
2022-02-16 23:26:28 +0900

    FIx: user migration

--- app/cmd/migrations/sql/000001_create_user.up.sql
+++ app/cmd/migrations/sql/000001_create_user.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-    `user_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'ユーザID',
+    `display_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '表示名',
     `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '氏名',
     `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'メールアドレス',
     `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'パスワード',
@@ -8,6 +8,6 @@ CREATE TABLE users (
     `updated_at` TIMESTAMP COMMENT '更新日時',
     `deleted_at` TIMESTAMP COMMENT '更新日時',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uniq_users_on_user_id` (`user_id`),
+    UNIQUE KEY `uniq_users_on_display_id` (`display_id`),
     UNIQUE KEY `uniq_users_on_email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
